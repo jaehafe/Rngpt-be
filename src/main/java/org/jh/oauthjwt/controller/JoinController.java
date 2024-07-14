aa3e1083742368117c0cf20edcaa15bfabb7cece
Jaeha Lee <wogkdkrm112@gmail.com>
2024-07-14 22:52:45 +0900

    chore: minor update

--- src/main/java/org/jh/oauthjwt/controller/JoinController.java
+++ src/main/java/org/jh/oauthjwt/controller/JoinController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -31,12 +30,4 @@ public class JoinController {
         String result = joinService.verifyEmail(verifyDTO);
         return ResponseEntity.ok(result);
     }
-
-//    @PostMapping("/join")
-//    public String joinProcess(@RequestBody JoinDTO joinDTO) {
-//
-//        joinService.joinProcess(joinDTO);
-//
-//        return "ok";
-//    }
 }
