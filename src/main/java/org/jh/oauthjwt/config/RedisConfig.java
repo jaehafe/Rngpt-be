10cd8d03e5bcceb35a264c41af0ee0b04ef4bc7f
jaehafe <wogkdkrm112@gmail.com>
2024-07-15 16:26:27 +0900

    chore: minor update

--- src/main/java/org/jh/oauthjwt/config/RedisConfig.java
+++ src/main/java/org/jh/oauthjwt/config/RedisConfig.java
@@ -10,18 +10,6 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-//@Configuration
-//public class RedisConfig {
-//    @Bean
-//    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-//        RedisTemplate<String, String> template = new RedisTemplate<>();
-//        template.setConnectionFactory(connectionFactory);
-//        template.setKeySerializer(new StringRedisSerializer());
-//        template.setValueSerializer(new StringRedisSerializer());
-//        return template;
-//    }
-//}
-
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
