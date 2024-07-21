bbbd86bb221c4b87c6bb422011f27ae67de50ce9
Jaeha Lee <wogkdkrm112@gmail.com>
2024-07-21 12:31:47 +0900

    feat: createTodo

--- src/main/java/org/jh/oauthjwt/todo/presentation/TodoController.java
+++ src/main/java/org/jh/oauthjwt/todo/presentation/TodoController.java
@@ -1,10 +1,13 @@
 package org.jh.oauthjwt.todo.presentation;
 
 import java.util.List;
+import org.jh.oauthjwt.todo.dto.request.TodoRequest;
 import org.jh.oauthjwt.todo.dto.response.TodoResponse;
 import org.jh.oauthjwt.todo.service.TodoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +25,11 @@ public class TodoController {
     public ResponseEntity<List<TodoResponse>> getTodos() {
         final List<TodoResponse> todoResponses = todoService.getAllTodos();
         return ResponseEntity.ok(todoResponses);
+    }
+
+    @PostMapping("/todos")
+    public ResponseEntity<Void> createTodo(@RequestBody final TodoRequest todoRequest) {
+        todoService.createTodo(todoRequest);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/org/jh/oauthjwt/todo/service/TodoService.java
+++ src/main/java/org/jh/oauthjwt/todo/service/TodoService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.jh.oauthjwt.todo.domain.Todo;
 import org.jh.oauthjwt.todo.domain.repository.TodoRepository;
+import org.jh.oauthjwt.todo.dto.request.TodoRequest;
 import org.jh.oauthjwt.todo.dto.response.TodoResponse;
 import org.springframework.stereotype.Service;
 
@@ -25,5 +26,10 @@ public class TodoService {
         return todos.stream()
                 .map(TodoResponse::of)
                 .toList();
+    }
+
+    // todo 생성
+    public void createTodo(final TodoRequest todoRequest) {
+        todoRepository.save(Todo.of(todoRequest));
     }
 }
